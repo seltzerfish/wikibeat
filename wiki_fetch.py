471720f339b0dc75342fc44df5c0af225248d125
Conner Lane <cal0043@auburn.edu>
2018-10-20 02:52:53 -0400

    initial

---
+++ wiki_fetch.py
@@ -0,0 +1,14 @@
+import lxml
+import bs4
+import urllib
+
+
+def fetch_page_content(url):
+    data = urllib.request.urlopen(url)
+    content = data.read()
+    parsed_content = bs4.BeautifulSoup(content,'lxml')
+    paragraphs = parsed_content.find_all('p')
+    article_text = "" 
+    for p in paragraphs:  
+        article_text += p.text
+    return article_text.replace("\n", " ")
